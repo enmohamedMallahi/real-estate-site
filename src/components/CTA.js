d75371c998b53337062156cc715a291267bf31d6
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-09-06 21:05:21 +0100

    fix z-index pages

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="relative grid place-content-center bg-no-repeat bg-cover" style={{ backgroundImage: 'url(https://www.immomonte-carlo.com/wp-content/uploads/2018/12/Compromis-de-vente.jpg)' }}>
       <div className="absolute top-0 right-0 left-0 bottom-0 bg-black opacity-50"></div>
-      <div className="text-white text-center z-50 px-8 md:px-8 py-24">
+      <div className="text-white text-center z-10 px-8 md:px-8 py-24">
 
         <h2 className="text-2xl font-bold  mb-4">Louer, acheter ou vendre</h2>
         <p className="mb-4">Vous cherchez à louer ou à acheter une nouvelle propriété ou à vendre une existante?<br /> L'agence Immo Monte-Carlo fournit une solution facile!</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-gray-100 sticky top-0 right-0 left-0 z-[150]">
+    <header className="bg-gray-100 sticky top-0 right-0 left-0 z-[100]">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5">
@@ -59,9 +59,9 @@ export default function Example() {
         </Popover.Group>
 
       </nav>
-      <Dialog as="div" className="lg:hidden z-[200]" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog as="div" className="lg:hidden z-[150]" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-[200] w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-[150] w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="/" className="-m-1.5 p-1.5">
               <>
