c431a5cff261375935e2d977cb4390df255f9376
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-09-06 19:49:08 +0100

    f

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default async function Home() {
   return (
     <>
       {/* <Hero /> */}
-      <PropertySearch properties={properties} />
+      <PropertySearch properties={[]} />
       <section className="px-4">
         <h2 className="text-2xl text-center mb-2 font-bold">Bienvenue dans votre agence <br /> <span className="text-4xl">Immo Monte-Carlo</span></h2>
         <p className="text-center text-gray-500 mb-8">Notre Agence Immo Monte-Carlo vous accompagne dans tous vos projets immobiliers . Nous sommes spécialisés dans la vente, l'achat et la location de tous type de biens (appartements, maisons, villas, garages, terrains, bureaux et locaux commerciaux...) sur rabat et régions.</p>
