bd4b3ca801436b52b14b39af101b0e88388e4c2c
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-09-06 20:54:14 +0100

    u

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -59,7 +59,7 @@ export default function Example() {
         </Popover.Group>
 
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog as="div" className="lg:hidden z-[200]" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
