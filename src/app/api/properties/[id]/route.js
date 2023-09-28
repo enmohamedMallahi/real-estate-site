7f0e4a836e2917b5f1433e1debb755a1000abf9d
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-09-28 20:05:33 +0100

    fix: remove api folder

--- src/app/api/properties/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import properties from '@/data/properties'
-
-export async function GET(request, { params }) {
-
-  // console.log(, params.id)
-  return NextResponse.json(properties.filter(prp => prp.id == params.id)[0])
-}
