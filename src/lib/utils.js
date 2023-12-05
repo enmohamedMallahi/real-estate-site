df4573f51be750510f11f4905b2d11eee02d0132
enmohamedMallahi <mohaxxmall@ygmail.com>
2023-12-05 20:32:56 +0100

    fix

---
+++ src/lib/utils.js
@@ -0,0 +1,18 @@
+export const locations = [
+  'Aïn Chock',
+  'Aïn Sebaâ',
+  'Anfa',
+  'Ben M\'Sick',
+  'Sidi Bernoussi',
+  'Al Fida - Mers Sultan',
+  'Hay Hassani',
+  'Moulay Rachid'
+]
+
+export const propertyTypes = [
+  'appartement',
+  'studio',
+  'bureau',
+  'magasin',
+  'terrain'
+]
